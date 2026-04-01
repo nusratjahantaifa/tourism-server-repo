53eb3f2689273d313819ff317856afec199a2094
nusratjahantaifa <nusratjahantaifa05@gmail.com>
2026-04-01 15:53:15 +0600

    my commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    console.log("Connected to MongoDB");
+   
     //collection create
       const tourismCollection = client
       .db("tourismDB")
@@ -96,7 +96,7 @@ app.delete ('/spots/:id',
 
     }
 )
-   
+    console.log("Connected to MongoDB");
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
